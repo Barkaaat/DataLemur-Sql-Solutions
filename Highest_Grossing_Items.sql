c072a617eeba539e38e9be5df14bb3c0aa4a912f
Ahmed Barakat <ahmed0barakat001@gmail.com>
2024-11-10 17:01:25 +0200

    medium-Amazon

---
+++ Highest_Grossing_Items.sql
@@ -0,0 +1,14 @@
+-- question link --https://datalemur.com/questions/sql-highest-grossing
+
+select category, product, total_spend
+from (
+	select 
+		category, 
+		product, 
+		sum(spend) as total_spend, 
+		dense_rank() over (partition by category order by sum(spend) desc) as ds
+	from product_spend
+	where extract(year from transaction_date) = 2022
+	group by product, category
+) as T
+where T.ds < 3
