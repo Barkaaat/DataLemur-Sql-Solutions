08ce95b19cd7ad7b762a423a66583ca575ef3caa
Ahmed Barakat <ahmed0barakat001@gmail.com>
2024-11-12 20:45:21 +0200

    medium-FAANG

--- Top_Three_Salaries.sql
+++ Top_Three_Salaries.sql
@@ -1,15 +1,17 @@
 -- question link -- https://datalemur.com/questions/sql-top-three-salaries
 
+with emp as (
+	select *, dense_rank() over(partition by department_id order by salary desc) as "dr"
+	from employee
+	order by salary desc
+)
+
 select
 	d.department_name,
 	e.name,
 	e.salary
 from 
-	(
-		select *, dense_rank() over(partition by department_id order by salary desc) as "dr"
-		from employee
-		order by salary desc
-	) as e
+	emp e
 right join
 	department d
 on d.department_id = e.department_id
