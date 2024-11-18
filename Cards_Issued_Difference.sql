6d63274a79f12e738bda0d3110a3b44b4d640bca
Ahmed Barakat <ahmed0barakat001@gmail.com>
2024-11-18 22:49:37 +0200

    easy-JPMorgan Chase

---
+++ Cards_Issued_Difference.sql
@@ -0,0 +1,6 @@
+-- question link -- https://datalemur.com/questions/cards-issued-difference
+
+select card_name, max(issued_amount)-min(issued_amount) difference
+from monthly_cards_issued
+group by card_name
+order by difference desc;
